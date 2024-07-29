a5443488d5ff2c09831bf1a409507a4d47448e5e
gabrafo <gafagundesmesquita@gmail.com>
2024-07-29 16:51:06 -0300

    updating sprites

--- src/entities/Entity.java
+++ src/entities/Entity.java
@@ -7,7 +7,10 @@ public class Entity {
     private int x, y;
     private int speed;
 
-    private BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
+    private BufferedImage up1, up2, up3, up4, up5, up6, up7, up8,
+            down1, down2,down3, down4, down5, down6, down7, down8,
+            left1, left2, left3, left4, left5, left6, left7, left8,
+            right1, right2, right3, right4, right5, right6, right7, right8;
     private String direction;
     private int spriteCounter = 0;
     private int spriteNum = 1;
@@ -68,6 +71,54 @@ public class Entity {
         this.up2 = up2;
     }
 
+    public BufferedImage getUp3() {
+        return up3;
+    }
+
+    public void setUp3(BufferedImage up3) {
+        this.up3 = up3;
+    }
+
+    public BufferedImage getUp4() {
+        return up4;
+    }
+
+    public void setUp4(BufferedImage up4) {
+        this.up4 = up4;
+    }
+
+    public BufferedImage getUp5() {
+        return up5;
+    }
+
+    public void setUp5(BufferedImage up5) {
+        this.up5 = up5;
+    }
+
+    public BufferedImage getUp6() {
+        return up6;
+    }
+
+    public void setUp6(BufferedImage up6) {
+        this.up6 = up6;
+    }
+
+    public BufferedImage getUp7() {
+        return up7;
+    }
+
+    public void setUp7(BufferedImage up7) {
+        this.up7 = up7;
+    }
+
+    public BufferedImage getUp8() {
+        return up8;
+    }
+
+    public void setUp8(BufferedImage up8) {
+        this.up8 = up8;
+    }
+
     public BufferedImage getDown1() {
         return down1;
     }
@@ -82,6 +133,54 @@ public class Entity {
 
     public void setDown2(BufferedImage down2) {
         this.down2 = down2;
+    }
+
+    public BufferedImage getDown3() {
+        return down3;
+    }
+
+    public void setDown3(BufferedImage down3) {
+        this.down3 = down3;
+    }
+
+    public BufferedImage getDown4() {
+        return down4;
+    }
+
+    public void setDown4(BufferedImage down4) {
+        this.down4 = down4;
+    }
+
+    public BufferedImage getDown5() {
+        return down5;
+    }
+
+    public void setDown5(BufferedImage down5) {
+        this.down5 = down5;
+    }
+
+    public BufferedImage getDown6() {
+        return down6;
+    }
+
+    public void setDown6(BufferedImage down6) {
+        this.down6 = down6;
+    }
+
+    public BufferedImage getDown7() {
+        return down7;
+    }
+
+    public void setDown7(BufferedImage down7) {
+        this.down7 = down7;
+    }
+
+    public BufferedImage getDown8() {
+        return down8;
+    }
+
+    public void setDown8(BufferedImage down8) {
+        this.down8 = down8;
     }
 
     public BufferedImage getLeft1() {
@@ -100,7 +199,55 @@ public class Entity {
         this.left2 = left2;
     }
 
-    public BufferedImage getRight1() {
+    public BufferedImage getLeft3() {
+        return left3;
+    }
+
+    public void setLeft3(BufferedImage left3) {
+        this.left3 = left3;
+    }
+
+    public BufferedImage getLeft4() {
+        return left4;
+    }
+
+    public void setLeft4(BufferedImage left4) {
+        this.left4 = left4;
+    }
+
+    public BufferedImage getLeft5() {
+        return left5;
+    }
+
+    public void setLeft5(BufferedImage left5) {
+        this.left5 = left5;
+    }
+
+    public BufferedImage getLeft6() {
+        return left6;
+    }
+
+    public void setLeft6(BufferedImage left6) {
+        this.left6 = left6;
+    }
+
+    public BufferedImage getLeft7() {
+        return left7;
+    }
+
+    public void setLeft7(BufferedImage left7) {
+        this.left7 = left7;
+    }
+
+    public BufferedImage getLeft8() {
+        return left8;
+    }
+
+    public void setLeft8(BufferedImage left8) {
+        this.left8 = left8;
+    }
+
+    public BufferedImage getRight1() { getRight8();
         return right1;
     }
 
@@ -114,6 +261,54 @@ public class Entity {
 
     public void setRight2(BufferedImage right2) {
         this.right2 = right2;
+    }
+
+    public BufferedImage getRight3() {
+        return right3;
+    }
+
+    public void setRight3(BufferedImage right3) {
+        this.right3 = right3;
+    }
+
+    public BufferedImage getRight4() {
+        return right4;
+    }
+
+    public void setRight4(BufferedImage right4) {
+        this.right4 = right4;
+    }
+
+    public BufferedImage getRight5() {
+        return right5;
+    }
+
+    public void setRight5(BufferedImage right5) {
+        this.right5 = right5;
+    }
+
+    public BufferedImage getRight6() {
+        return right6;
+    }
+
+    public void setRight6(BufferedImage right6) {
+        this.right6 = right6;
+    }
+
+    public BufferedImage getRight7() {
+        return right7;
+    }
+
+    public void setRight7(BufferedImage right7) {
+        this.right7 = right7;
+    }
+
+    public BufferedImage getRight8() {
+        return right8;
+    }
+
+    public void setRight8(BufferedImage right8) {
+        this.right8 = right8;
     }
 
     public String getDirection() {
--- src/entities/Player.java
+++ src/entities/Player.java
@@ -29,14 +29,38 @@ public class Player extends Entity {
 
     public void getPlayerImage(){
         try {
-            setUp1(ImageIO.read(getClass().getResourceAsStream("/player/boy_up_1.png")));
-            setUp2(ImageIO.read(getClass().getResourceAsStream("/player/boy_up_2.png")));
-            setDown1(ImageIO.read(getClass().getResourceAsStream("/player/boy_down_1.png")));
-            setDown2(ImageIO.read(getClass().getResourceAsStream("/player/boy_down_2.png")));
-            setLeft1(ImageIO.read(getClass().getResourceAsStream("/player/boy_left_1.png")));
-            setLeft2(ImageIO.read(getClass().getResourceAsStream("/player/boy_left_2.png")));
-            setRight1(ImageIO.read(getClass().getResourceAsStream("/player/boy_right_1.png")));
-            setRight2(ImageIO.read(getClass().getResourceAsStream("/player/boy_right_2.png")));
+            setUp1(ImageIO.read(getClass().getResourceAsStream("/player/bundle/up_1.png")));
+            setUp2(ImageIO.read(getClass().getResourceAsStream("/player/bundle/up_2.png")));
+            setUp3(ImageIO.read(getClass().getResourceAsStream("/player/bundle/up_3.png")));
+            setUp4(ImageIO.read(getClass().getResourceAsStream("/player/bundle/up_4.png")));
+            setUp5(ImageIO.read(getClass().getResourceAsStream("/player/bundle/up_5.png")));
+            setUp6(ImageIO.read(getClass().getResourceAsStream("/player/bundle/up_6.png")));
+            setUp7(ImageIO.read(getClass().getResourceAsStream("/player/bundle/up_7.png")));
+            setUp8(ImageIO.read(getClass().getResourceAsStream("/player/bundle/up_8.png")));
+            setDown1(ImageIO.read(getClass().getResourceAsStream("/player/bundle/down_1.png")));
+            setDown2(ImageIO.read(getClass().getResourceAsStream("/player/bundle/down_2.png")));
+            setDown3(ImageIO.read(getClass().getResourceAsStream("/player/bundle/down_3.png")));
+            setDown4(ImageIO.read(getClass().getResourceAsStream("/player/bundle/down_4.png")));
+            setDown5(ImageIO.read(getClass().getResourceAsStream("/player/bundle/down_5.png")));
+            setDown6(ImageIO.read(getClass().getResourceAsStream("/player/bundle/down_6.png")));
+            setDown7(ImageIO.read(getClass().getResourceAsStream("/player/bundle/down_7.png")));
+            setDown8(ImageIO.read(getClass().getResourceAsStream("/player/bundle/down_8.png")));
+            setLeft1(ImageIO.read(getClass().getResourceAsStream("/player/bundle/left_1.png")));
+            setLeft2(ImageIO.read(getClass().getResourceAsStream("/player/bundle/left_2.png")));
+            setLeft3(ImageIO.read(getClass().getResourceAsStream("/player/bundle/left_3.png")));
+            setLeft4(ImageIO.read(getClass().getResourceAsStream("/player/bundle/left_4.png")));
+            setLeft5(ImageIO.read(getClass().getResourceAsStream("/player/bundle/left_5.png")));
+            setLeft6(ImageIO.read(getClass().getResourceAsStream("/player/bundle/left_6.png")));
+            setLeft7(ImageIO.read(getClass().getResourceAsStream("/player/bundle/left_7.png")));
+            setLeft8(ImageIO.read(getClass().getResourceAsStream("/player/bundle/left_8.png")));
+            setRight1(ImageIO.read(getClass().getResourceAsStream("/player/bundle/right_1.png")));
+            setRight2(ImageIO.read(getClass().getResourceAsStream("/player/bundle/right_2.png")));
+            setRight3(ImageIO.read(getClass().getResourceAsStream("/player/bundle/right_3.png")));
+            setRight4(ImageIO.read(getClass().getResourceAsStream("/player/bundle/right_4.png")));
+            setRight5(ImageIO.read(getClass().getResourceAsStream("/player/bundle/right_5.png")));
+            setRight6(ImageIO.read(getClass().getResourceAsStream("/player/bundle/right_6.png")));
+            setRight7(ImageIO.read(getClass().getResourceAsStream("/player/bundle/right_7.png")));
+            setRight8(ImageIO.read(getClass().getResourceAsStream("/player/bundle/right_8.png")));
         }catch(IOException e){
             e.printStackTrace();
         }
@@ -66,11 +90,11 @@ public class Player extends Entity {
         if(keyPressed){
             setSpriteCounter(getSpriteCounter()+1);
             if(getSpriteCounter()>12){
-                if(getSpriteNum()==1){
-                    setSpriteNum(2);
-                }
-                else if(getSpriteNum()==2){
+                if(getSpriteNum()==8){
                     setSpriteNum(1);
+                }
+                else {
+                    setSpriteNum(getSpriteNum() + 1);
                 }
                 setSpriteCounter(0);
             }
@@ -79,39 +103,52 @@ public class Player extends Entity {
 
     public void draw(Graphics g2d){
         BufferedImage image = null;
-        switch (getDirection()) {
-            case "up":
-                if(getSpriteNum() == 1){
-                    image = getUp1();
-                }
-                if(getSpriteNum() == 2){
-                    image = getUp2();
-                }
-                break;
-            case "down":
-                if(getSpriteNum() == 1){
-                    image = getDown1();
-                }
-                if(getSpriteNum() == 2){
-                    image = getDown2();
-                }
-                break;
-            case "left":
-                if(getSpriteNum() == 1){
-                    image = getLeft1();
-                }
-                if(getSpriteNum() == 2){
-                    image = getLeft2();
-                }
-                break;
-            case "right":
-                if(getSpriteNum() == 1){
-                    image = getRight1();
-                }
-                if(getSpriteNum() == 2){
-                    image = getRight2();
-                }
-                break;
+        image = switch (getDirection()) {
+            case "up" -> switch (getSpriteNum()) {
+                case 1 -> getUp1();
+                case 2 -> getUp2();
+                case 3 -> getUp3();
+                case 4 -> getUp4();
+                case 5 -> getUp5();
+                case 6 -> getUp6();
+                case 7 -> getUp7();
+                case 8 -> getUp8();
+                default -> image;
+            };
+            case "down" -> switch (getSpriteNum()) {
+                case 1 -> getDown1();
+                case 2 -> getDown2();
+                case 3 -> getDown3();
+                case 4 -> getDown4();
+                case 5 -> getDown5();
+                case 6 -> getDown6();
+                case 7 -> getDown7();
+                case 8 -> getDown8();
+                default -> image;
+            };
+            case "left" -> switch (getSpriteNum()) {
+                case 1 -> getLeft1();
+                case 2 -> getLeft2();
+                case 3 -> getLeft3();
+                case 4 -> getLeft4();
+                case 5 -> getLeft5();
+                case 6 -> getLeft6();
+                case 7 -> getLeft7();
+                case 8 -> getLeft8();
+                default -> image;
+            };
+            case "right" -> switch (getSpriteNum()) {
+                case 1 -> getRight1();
+                case 2 -> getRight2();
+                case 3 -> getRight3();
+                case 4 -> getRight4();
+                case 5 -> getRight5();
+                case 6 -> getRight6();
+                case 7 -> getRight7();
+                case 8 -> getRight8();
+                default -> image;
+            };
+            default -> null;
         };
 
         g2d.drawImage(image, getX(), getY(), gp.getTileSize(), gp.getTileSize(), null);
